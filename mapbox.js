eac8e5d67ed49090818da115ce8efe1a7fb7af2b
Björn Harrtell <bjorn.harrtell@sweco.se>
2013-05-08 10:52:49 +0200

    Initial commit

---
+++ mapbox.js
@@ -0,0 +1,23 @@
+var osm = new OpenLayers.Layer.XYZ(
+    "osm",
+    [
+        "http://a.tiles.mapbox.com/v3/bjornharrtell.map-em5abk65/${z}/${x}/${y}.png",
+        "http://b.tiles.mapbox.com/v3/bjornharrtell.map-em5abk65/${z}/${x}/${y}.png",
+        "http://c.tiles.mapbox.com/v3/bjornharrtell.map-em5abk65/${z}/${x}/${y}.png",
+        "http://d.tiles.mapbox.com/v3/bjornharrtell.map-em5abk65/${z}/${x}/${y}.png"
+    ], {
+        attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
+        sphericalMercator: true,
+        wrapDateLine: true
+    }
+);
+
+var map = new OpenLayers.Map({
+    div: "map",
+    layers: [osm],
+    center: [1721973.373208, 9047015.384574],
+    zoom: 5,
+    controls: [new OpenLayers.Control.Navigation(), new OpenLayers.Control.Attribution()]
+});
+
+
